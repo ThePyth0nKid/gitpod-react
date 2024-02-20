5d83be64eafdaf78fb56055f9b00596dd1098bc8
Nelson Mehlis <Nelson.mehlis@icloud.com>
2024-02-20 11:29:48 +0000

    Add callback to handleClick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FunctionalGreeting from "./components/FunctionalGreeting"
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps"
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" />
       <StatefulGreeting greeting="I'm a stateful class component!" />
+      <StatefulGreetingWithCallback greeting="I'm a stateful class with component component!" />
     </div>
   );
 }
--- my-app/src/components/StatefulGreetingWithCallback.js
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class StatefulGreeting extends React.Component {
+class StatefulGreetingWithCallback extends React.Component {
 
     constructor(props) {
         super(props);
@@ -13,8 +13,13 @@ class StatefulGreeting extends React.Component {
     handleClick() {
         this.setState({
             introduction: "Goodbye!",
+            buttonText: "Enter",
+        }, ()=>{
+            console.log("new state", this.state.introduction);
+            console.log("new state", this.state.buttonText);    
         });
         console.log(this.state.introduction);
+        console.log(this.state.buttonText);
     }
 
     render () {
@@ -28,4 +33,4 @@ class StatefulGreeting extends React.Component {
     }
 }
 
-export default StatefulGreeting;
+export default StatefulGreetingWithCallback;
