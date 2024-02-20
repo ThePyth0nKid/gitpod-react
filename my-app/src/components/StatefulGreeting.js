babef61cf429e087a7c735262445c44c422b88a3
Nelson Mehlis <Nelson.mehlis@icloud.com>
2024-02-20 09:52:40 +0000

    Add stateful component to StatefulGreeting.js

---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    render () {
+        //return <h1>Hello from the stateful class component!</h1>
+        return (
+            <div>
+                <h1>{this.state.introduction} from the stateful class component!</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting;
