4706bbc2e68393e934a0df3217200d131eb15e7d
Nelson Mehlis <Nelson.mehlis@icloud.com>
2024-02-21 09:16:25 +0000

    Add EventClass.js and EventFunctional.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <StatefulGreeting greeting="I'm a stateful class component!" />
       <StatefulGreetingWithCallback />
       <StatefulGreetingWithPrevState />
+      <EventsClass />
+      <EventsFunctional />
     </div>
   );
 }
